43dff9a46e763889499e0320580d8cf09a24fc97
João Severo <joaovbsevero@gmail.com>
2025-04-20 20:46:37 -0300

    .

--- dspbench-flink/data/VoIP.py
+++ dspbench-flink/data/VoIP.py
@@ -44,4 +44,3 @@ for i in range(800000):
 
     #calling,called,answerTime,calling;called;answerTime;duration;estabilished
     file.write(CallingNumber+','+CalledNumber+','+AnswerTime.strftime('%Y-%m-%dT%H:%M:%S')+','+CallingNumber+';'+CalledNumber+';'+AnswerTime.strftime('%Y-%m-%dT%H:%M:%S')+';'+str(CallDuration)+';'+CallEstablished+"\n")
-    
